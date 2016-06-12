22fc6424ab16e92cafd3b22c8a395ba235f4ef69
Eugene M <eugene.m92@gmail.com>
2016-06-12 10:06:53 +0500

    now using requests library instead of native http/httpd library

--- geoip.js
+++ geoip.js
@@ -1,39 +1,23 @@
 var fs = require('fs');
-
-var http = require('http');
+var request = require('request');
 var cache = require('./geoip.json');
 
 var lookup = function(host, done, fuck) {
 	if (typeof(cache[host]) == 'undefined') {
-		/*
-		var options = {
-			host: 'ip-api.com',
-			port: 80,
-			path: "/json/" + host,
-		};
-		// */
-		// if you are banned for some reason in ip-api.com, use webproxy like below
-		//*
-		var options = {
-			host: 'new.great-windmill.com',
-			port: 80,
-			path: "/js/upload/browse.php?u=http%3A%2F%2Fip-api.com%2Fjson%2F" + host,
-		};
-		// 
-		http.get(options, function(response) {
-			var data = "";
-			response.on("data", function(chunk) {
-				data += chunk;
-			});
-			response.on("end", function() {
-				data = JSON.parse(data);
-				cache[host] = {country: data.countryCode, city: data.city.split(" (")[0]};
-				console.log(cache[host]);
-				fs.writeFile("./geoip.json", JSON.stringify(cache));
-				done(cache[host]);
-			});
-		}).on('error', function(yourself) {
-			fuck(yourself);
+		request('http://ip-api.com/json/' + host, {timeout: 5000}, function (error, response, body) {
+			if (error) {
+				fuck(error);
+			} else {
+				if (response.statusCode != 200) {
+					fuck({message: "statustCode: " + response.statusCode.toString()});
+				} else {
+					data = JSON.parse(body);
+					cache[host] = {country: data.countryCode, city: data.city.split(" (")[0]};
+					console.log(cache[host]);
+					fs.writeFile("./geoip.json", JSON.stringify(cache));
+					done(cache[host]);
+				}
+			}
 		});
 	} else {
 		done(cache[host]);
--- master.js
+++ master.js
@@ -1,23 +1,21 @@
-var https = require('https');
+var request = require('request');
 
 var query = function(done, fuck) {
 	console.log("Querying syncore's server list");
-	https.get('https://ql.syncore.org/api/servers', function(res) {
-		var data = "";
-		res.on('data', function(chunk) {
-			data += chunk;
-		});
-		res.on("end", function() {
-			data = JSON.parse(data);
-			servers = data.servers.map(function(item) {
-				return item.address;
-			});
-			
-			done(servers);
-		});
-	
-	}).on('error', function(yourself) {
-		fuck(yourself);
+	request('https://ql.syncore.org/api/servers', {timeout: 5000}, function (error, response, body) {
+		if (error) {
+			fuck(error);
+		} else {
+			if (response.statusCode != 200) {
+				fuck({message: "statustCode: " + response.statusCode.toString()});
+			} else {
+				data = JSON.parse(body);
+				servers = data.servers.map(function(item) {
+					return item.address;
+				});
+				done(servers);
+			}
+		}
 	});
 };
 
