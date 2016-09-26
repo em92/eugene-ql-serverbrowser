8d2275814107b01ba0fb4d34adb695293e1da761
Eugene Molotov <eugene.m92@gmail.com>
2016-09-26 10:35:24 +0500

    removed server_ips property

--- skillrating.js
+++ skillrating.js
@@ -1,6 +1,5 @@
 var rp = require('request-promise');
 
-var server_ips = {};
 var skill_rating = {};
 
 var query = function() {
@@ -17,16 +16,12 @@ var query = function() {
     data.forEach(function(item) {
       var address = item.server;
       delete item.server;
-      server_ips[ address.split(":")[0] ] = 1;
       skill_rating[ address ] = item;
     });
   });
 };
 
 module.exports.query = query;
-Object.defineProperty(module.exports, "server_ips", {
-  get: function() { return server_ips; }
-});
 Object.defineProperty(module.exports, "skill_rating", {
   get: function() { return skill_rating; }
 });
