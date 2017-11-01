707b59ac42587f4d51f98e5fe99ee473de2b6d75
Eugene Molotov <em92@users.noreply.github.com>
2017-11-01 16:06:20 +0500

    correcting zmq ports for some servers

--- skillrating.js
+++ skillrating.js
@@ -2,6 +2,32 @@ var rp = require('request-promise');
 var error_handler = require('./master.js').rp_error_handler;
 
 var skill_rating = {};
+var ZMQ_TO_GAME_ADDR_DATA = {
+  '109.94.1.176': -1000, // 125FPS Podolsk/Moscow
+  '120.76.221.244': -2000 // Moon (China)
+};
+var GAME_TO_ZMQ_ADDR_DICT = {};
+
+var convert_zmq_to_game_addr = function( addr ) {
+  try {
+    var host = addr.split(":")[0];
+
+    if ( ZMQ_TO_GAME_ADDR_DATA[ host ] ) {
+      var port = parseInt(addr.split(":")[1]) + ZMQ_TO_GAME_ADDR_DATA[ host ];
+      var game_addr = host + ":" + port.toString();
+      GAME_TO_ZMQ_ADDR_DICT[ game_addr ] = addr;
+      return game_addr;
+    } else {
+      return addr;
+    }
+  } catch(e) {
+    return addr;
+  }
+};
+
+var convert_game_to_zmq_addr = function( addr ) {
+  return GAME_TO_ZMQ_ADDR_DICT[ addr ] ? GAME_TO_ZMQ_ADDR_DICT[ addr ] : addr;
+};
 
 var query = function() {
   var options = {
@@ -20,7 +46,7 @@ var query = function() {
     server_ips = {};
     skill_rating = {}
     data.forEach(function(item) {
-      var address = item.server;
+      var address = convert_zmq_to_game_addr( item.server );
       delete item.server;
       skill_rating[ address ] = item;
     });
@@ -30,7 +56,7 @@ var query = function() {
 
 var query_server_players = function( address ) {
   var options = {
-    uri: 'http://api.qlstats.net/api/server/' + address + '/players',
+    uri: 'http://api.qlstats.net/api/server/' + convert_game_to_zmq_addr( address ) + '/players',
     timeout: 5000,
     json: true
   };
