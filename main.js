509a6803f02e1f1c4d7e93f55e3575ef75802bb7
Eugene Molotov <em92@users.noreply.github.com>
2018-02-07 16:01:33 +0500

    fixed screen path on npm start-dev

--- main.js
+++ main.js
@@ -145,8 +145,8 @@ app.get('/serverinfo2/:endpoints', function (req, res) {
 if (process.env.npm_lifecycle_event == "start-dev") {
   var fs = require("fs");
   var index_file_data = fs.readFileSync(__dirname + '/public/index.html', {encoding: 'utf8'}).replace(
-    '<script type="text/javascript" src="/js/serverbrowser.js?1"></script>',
-    '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"></script><script type="text/babel" src="/js/serverbrowser.babel.js"></script>'
+    '<script type="text/javascript" src="/js/serverbrowser.js?',
+    '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"></script><script type="text/babel" src="/js/serverbrowser.babel.js?'
   ).replace('.min.js', '.js');
   app.get('/', function (req, res) {
     res.setHeader("Content-Type", "text/html");
