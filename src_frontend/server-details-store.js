33a80c3e9c3aeb5765253abeebca83b11db87f7a
Eugene Molotov <em92@users.noreply.github.com>
2019-06-03 17:04:03 +0500

    frontend: finished implementing server-details-store

--- src_frontend/server-details-store.js
+++ src_frontend/server-details-store.js
@@ -1,3 +1,17 @@
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
+import { servers } from './server-list-store.js';
 
-export const serverDetails = writable(null);
+export const chosenServerAddress = writable(null);
+export const serverDetails = derived(
+  [chosenServerAddress, servers],
+  ([$chosenServerAddress, $servers], set) => {
+    for(let i in $servers) {
+      let server = $servers[i];
+      if (server.host_address == $chosenServerAddress) {
+        set(server);
+        return;
+      }
+    }
+    set(null);
+  }
+);
