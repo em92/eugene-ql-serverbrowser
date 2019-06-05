22e1961a6a52505f571c1afd84cdb4915b1d01d5
Eugene Molotov <em92@users.noreply.github.com>
2019-06-05 17:36:41 +0500

    frontend: rewrote server-details store

--- src_frontend/server-details/store.js
+++ src_frontend/server-details/store.js
@@ -1,17 +1,76 @@
 import { derived, writable } from 'svelte/store';
 import { servers } from '../server-list-store.js';
 
-export const chosenServerAddress = writable(null);
-export const serverDetails = derived(
-  [chosenServerAddress, servers],
-  ([$chosenServerAddress, $servers], set) => {
+let cache = {};
+
+const serversAsDict = derived(
+  servers, $servers => {
+    let result = {};
     for(let i in $servers) {
       let server = $servers[i];
-      if (server.host_address == $chosenServerAddress) {
-        set(server);
+      result[server.host_address] = server;
+    }
+    cache = {};
+    return result;
+  }
+);
+
+export const chosenServerAddress = writable(null);
+export const serverDetails = derived(
+  [chosenServerAddress, serversAsDict],
+  ([$chosenServerAddress, $serversAsDict], set) => {
+
+    function updateServer(server, newData) {
+      let data = Object.assign({}, newData, server);
+      set(data);
+    }
+
+    function fetchQLStatsData(server) {
+      if (server.gameinfo.players.length == 0) {
+        updateServer(server, {
+          qlstats: {ok: true, players: []},
+          loading: false
+        });
+        return;
+      };
+
+      if (cache[server.host_address]) {
+        updateServer(server, {
+          qlstats: cache[server.host_address],
+          loading: false
+        });
         return;
       }
+
+      updateServer(server, {loading: true});
+
+      fetch("/qlstats/" + server.host_address)
+      .then( response => response.json())
+      .then( data => {
+        cache[server.host_address] = data;
+        updateServer(server, {
+          qlstats: data,
+          loading: false
+        });
+      })
+      .catch( error => {
+        console.error(error);
+        updateServer(server, {
+          loading: false
+        });
+      })
     }
-    set(null);
+
+    let server = $serversAsDict[$chosenServerAddress];
+    if (!server) {
+      set(null);
+      return;
+    }
+
+    if (server.qlstats) {
+      updateServer(server, {loading: false});
+    } else if (!server.loading) {
+      fetchQLStatsData(server);
+    }
   }
 );
