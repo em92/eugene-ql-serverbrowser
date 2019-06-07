1befb0017704798ec7b674e341f4fcbc0394a42b
Eugene Molotov <em92@users.noreply.github.com>
2019-06-07 16:30:38 +0500

    frontend: server list store is now derived from filter blocks

--- src_frontend/filter-blocks/store.js
+++ src_frontend/filter-blocks/store.js
@@ -1,3 +1,14 @@
 import { derived, writable } from 'svelte/store';
 
 export let filters = writable({"0default": {"gametype": ["any"]}});
+export let cleanFilters = derived(
+  filters, ($filters) => {
+    return Object.keys( $filters ).map( i => {
+      var state = $.extend({}, $filters[i]);
+      if (state.tags) { // list of tags must have AND logic
+        state.tags = state.tags.join();
+      }
+      return state;
+    });
+  }
+)
--- src_frontend/server-list-store.js
+++ src_frontend/server-list-store.js
@@ -1,13 +1,20 @@
 import { writable } from 'svelte/store';
+import { cleanFilters as filters } from './filter-blocks/store.js';
 
 export const servers = writable([]);
 export const loading = writable(true);
 export const pause = writable(false);
 
+let encodedFilterData = "";
 const PERIOD = 10000;
 
+filters.subscribe( data => {
+  encodedFilterData = window.btoa(JSON.stringify(data));
+  serverListUpdater();
+});
+
 function serverListUpdater() {
-  fetch("/serverlist")
+  fetch("/serverlist/" + encodedFilterData)
   .then( response => response.json())
   .then( data => {
     servers.set(data.servers);
