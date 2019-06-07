857b53691cfe831a3f21d32e937a82a7863e6414
Eugene Molotov <em92@users.noreply.github.com>
2019-06-07 16:36:44 +0500

    frontend: added isShowingRawFilter flag

--- src_frontend/filter-blocks/main.svelte
+++ src_frontend/filter-blocks/main.svelte
@@ -1,6 +1,10 @@
 <script>
   import { filters } from "./store.js";
   import RawFiltersText from "./raw.svelte";
+
+  let isShowingRawFilter = true;
 </script>
 
-<RawFiltersText />
+{#if isShowingRawFilter}
+  <RawFiltersText on:click={() => {isShowingRawFilter = !isShowingRawFilter}} />
+{/if}
