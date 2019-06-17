5740c8c89aa3440f8241346e2f33707b78f1e440
Eugene Molotov <em92@users.noreply.github.com>
2019-06-17 17:10:07 +0500

    frontend: rewrited server-list code

--- src_frontend/server-list/store.js
+++ src_frontend/server-list/store.js
@@ -1,36 +1,29 @@
-import { writable } from 'svelte/store';
+import { writable, readable, derived } from 'svelte/store';
 import { cleanFilters as filters } from '../filter-blocks/store.js';
 
-export const servers = writable([]);
 export const loading = writable(true);
-export const pause = writable(false);
-// TODO: added error state
+export const pause = writable(true);
 
-let encodedFilterData = "";
-const PERIOD = 10000;
+const time = readable(new Date(), set => {
+	const interval = setInterval(() => {
+		set(new Date());
+	}, 10000);
 
-filters.subscribe( data => {
-  encodedFilterData = window.btoa(JSON.stringify(data));
-  serverListUpdater();
+	return () => clearInterval(interval);
 });
 
-function serverListUpdater() {
-  fetch("/serverlist/" + encodedFilterData)
-  .then( response => response.json())
-  .then( data => {
-    servers.set(data.servers);
-    loading.set(false);
-  });
-}
+export const servers = derived(
+  [filters, pause, time],
+  ([$filters, $pause, $time], set) => {
+    if ($pause) return;
 
-let timer = null;
-
-pause.subscribe(value => {
-  if (value == true) {
-    clearInterval(timer);
-  } else {
-    loading.set(true);
-    serverListUpdater();
-    timer = setInterval(serverListUpdater, PERIOD)
+    let encodedFilterData = window.btoa(JSON.stringify($filters));
+    fetch("/serverlist/" + encodedFilterData)
+    .then( response => response.json())
+    .then( data => {
+      set(data.servers);
+      loading.set(false);
+    });
   }
-});
+)
+// TODO: added error state
